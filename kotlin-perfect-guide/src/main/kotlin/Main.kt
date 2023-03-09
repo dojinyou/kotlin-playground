e8269c1e6458d52c8c7b05bc5384b30189f7eade
dojinyou <djyou128@gmail.com>
2023-03-09 23:58:59 +0900

    main 초기화

--- kotlin-perfect-guide/src/main/kotlin/Main.kt
+++ kotlin-perfect-guide/src/main/kotlin/Main.kt
@@ -1,10 +1,2 @@
 fun main(args: Array<String>) {
-    val a = 1
-    var b = 0
-    val result = calculate(a, b)
-}
-
-fun calculate(a:Int, b:Int) = when {
-    a > b -> println("big a")
-    b > a -> println("big b")
 }
